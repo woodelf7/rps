d6fa7740837e776a3a8d81b4bc450110310349e1
woodelf7 <woodelf7@gmail.com>
2014-06-26 13:44:38 -0400

    Added Notes.txt and starters.py

---
+++ starters.py
@@ -0,0 +1,9 @@
+def subber(x, y):
+    """return y - x"""
+    return y - x
+
+def choice():
+    """returns choice in lowercase"""
+    choice = raw_input("Enter a choice: ")
+    return choice.lower()
+
